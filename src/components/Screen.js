c0d62c1413a3985faab4c90adef42b62e1f8393b
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-04 17:35:54 +0530

    add setting component

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 // import Menu from './Menu';
-import Games from './Games';
+// import Games from './Games';
+import Setting from './Setting';
 import '../assets/css/screen.css';
 
 class Screen extends Component {
   render() {
     return (
       <div className="screen-container">
-        <Games />
+        {/* <Menu />
+        <Games /> */}
+        <Setting />
       </div>
     );
   }
---
+++ src/components/Setting.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../assets/css/common.css';
+import '../assets/css/setting.css';
+
+class Setting extends Component {
+  render() {
+    return <div className="setting-container screen-item-container"></div>;
+  }
+}
+
+export default Setting;
