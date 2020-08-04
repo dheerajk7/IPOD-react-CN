59445338b0d8237dea800c5b4d2b06100eec83b3
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-04 22:34:28 +0530

    add menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,8 +3,43 @@ import '../assets/css/common.css';
 import '../assets/css/menu.css';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 'Music',
+    };
+  }
+
   render() {
-    return <div className="menu-container screen-item-container">Menu</div>;
+    return (
+      <div className="menu-container screen-item-container">
+        <div className="menu">
+          <div className="menu-heading">IPOD</div>
+          <ul className="menu-list-container">
+            {this.state.activeItem === 'Overflow' ? (
+              <li className="menu-list-item active">Overflow</li>
+            ) : (
+              <li className="menu-list-item">Overflow</li>
+            )}
+            {this.state.activeItem === 'Music' ? (
+              <li className="menu-list-item active">Music</li>
+            ) : (
+              <li className="menu-list-item">Music</li>
+            )}
+            {this.state.activeItem === 'Games' ? (
+              <li className="menu-list-item active">Games</li>
+            ) : (
+              <li className="menu-list-item">Games</li>
+            )}
+            {this.state.activeItem === 'Setting' ? (
+              <li className="menu-list-item active">Setting</li>
+            ) : (
+              <li className="menu-list-item">Setting</li>
+            )}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-// import Menu from './Menu';
+import Menu from './Menu';
 // import Games from './Games';
 // import Setting from './Setting';
-import Coverflow from './Coverflow';
+// import Coverflow from './Coverflow';
 import '../assets/css/screen.css';
 
 class Screen extends Component {
   render() {
     return (
       <div className="screen-container">
-        {/* <Menu />*/}
+        <Menu />
         {/* <Games /> */}
         {/* <Setting /> */}
-        <Coverflow />
+        {/* <Coverflow /> */}
       </div>
     );
   }
