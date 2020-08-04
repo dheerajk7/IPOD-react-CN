c6fee1da12c31a48060af42e98274978cc59535a
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-04 15:37:03 +0530

    add buttons to the container

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,9 +7,37 @@ class Buttons extends Component {
       <div className="button-container">
         <div className="button-region-container">
           <div className="inner-button-container">
-            <div className="up-button-container"></div>
-            <div className="center-button-container"></div>
-            <div className="down-button-container"></div>
+            <div className="up-button-container">
+              <div className="button menu-button">
+                <img
+                  src="https://image.flaticon.com/icons/svg/2099/2099153.svg"
+                  alt="menu-button"
+                ></img>
+              </div>
+            </div>
+            <div className="center-button-container">
+              <div className="button backward-button">
+                <img
+                  src="https://image.flaticon.com/icons/svg/56/56760.svg"
+                  alt="backward-button"
+                ></img>
+              </div>
+              <div className="button center-button"></div>
+              <div className="button forward-button">
+                <img
+                  src="https://image.flaticon.com/icons/svg/724/724927.svg"
+                  alt="forward-button"
+                ></img>
+              </div>
+            </div>
+            <div className="down-button-container">
+              <div className="button pause-play-button">
+                <img
+                  src="https://image.flaticon.com/icons/svg/64/64595.svg"
+                  alt="pause-play-button"
+                ></img>
+              </div>
+            </div>
           </div>
         </div>
       </div>
