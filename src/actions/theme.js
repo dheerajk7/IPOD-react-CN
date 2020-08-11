424163217a86e306c6b91fa0cc66d412ee35bd87
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-11 20:38:20 +0530

    add themes data

--- src/actions/theme.js
+++ src/actions/theme.js
@@ -1,1 +1,9 @@
 import CHANGE_THEME from './actionTypes';
+
+//action creator for theme
+export function changeTheme(theme) {
+  return {
+    type: CHANGE_THEME,
+    theme,
+  };
+}
---
+++ src/data/themes.js
@@ -0,0 +1,56 @@
+export const Marshmello = {
+  background: { backgroundColor: '#f5c8cf' },
+  ipod_background: {
+    backgroundImage: 'linear-gradient(to right, #396570, #1c7187, #184b58)',
+  },
+  circle_background: {
+    backgroundImage: 'linear-gradient(to right, #e5b17a, #c66d18, #f3be8c)',
+  },
+  menu: {
+    menu: {
+      backgroundColor: '#fcffe3a1',
+    },
+    active: {
+      backgroundImage: 'linear-gradient(to right, #cd2f0b, #b22100, #992409)',
+      color: 'white',
+    },
+  },
+};
+
+export const Striker = {
+  background: { backgroundColor: 'yellow' },
+  ipod_background: {
+    backgroundImage: 'linear-gradient(to right, blue, blue, black)',
+  },
+  circle_background: {
+    backgroundImage: 'linear-gradient(to right, red, red, orange)',
+  },
+  menu: {
+    menu: {
+      backgroundColor: 'white',
+    },
+    active: {
+      backgroundImage: 'linear-gradient(to right, green, green, pink)',
+      color: 'white',
+    },
+  },
+};
+
+export const Virtue = {
+  background: { backgroundColor: 'lightpink' },
+  ipod_background: {
+    backgroundImage: 'linear-gradient(to right, red, red, pink)',
+  },
+  circle_background: {
+    backgroundImage: 'linear-gradient(to right, black, black, black)',
+  },
+  menu: {
+    menu: {
+      backgroundColor: 'navy',
+    },
+    active: {
+      backgroundImage: 'linear-gradient(to right, orange, orange, blue)',
+      color: 'white',
+    },
+  },
+};
